96df9e84c91dc2a2625a0dafd8ff2e3b40d91129
小权一句两句 <xqyjlj@126.com>
2022-10-15 19:47:21 +0800

    补充make参数

--- ports/rtt-smart-riscv64/variants/standard/mpconfigvariant.mk
+++ ports/rtt-smart-riscv64/variants/standard/mpconfigvariant.mk
@@ -1,3 +1,12 @@
-# This is the default variant when you `make` the Unix port.
+TOOLCHAIN_ROOT = /home/xqyjlj/Downloads/riscv64-linux-musleabi_for_x86_64-pc-linux-gnu/bin/
+CROSS_COMPILE = $(TOOLCHAIN_ROOT)riscv64-unknown-linux-musl-
 
-FROZEN_MANIFEST ?= $(VARIANT_DIR)/manifest.py
+CC = $(TOOLCHAIN_ROOT)riscv64-unknown-linux-musl-gcc
+DB = $(TOOLCHAIN_ROOT)riscv64-unknown-linux-musl-gdb
+
+# 使用arm平台时, -mcmodel=medany参数要删除
+CFLAGS += -Wall -O0 -g --static -mcmodel=medany
+
+
+LD_FILES = $(shell pwd)/link.lds
+LDFLAGS += -static -T ${LD_FILES}
